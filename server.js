4f192d0fc8788d504fe0b968c6ebca7759b4e32d
Krystopher Quintero <krystopher.q@gmail.com>
2022-04-27 15:05:29 -0700

    npm install

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const PORT = process.envPORT || 3001;
 const fs = require('fs');
 const express = require('express');
 const app = express();
-
+const path = require('path')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,6 +16,7 @@ app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
+app.get()
 
 
 
