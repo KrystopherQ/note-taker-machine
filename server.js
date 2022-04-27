fe7dc20275df6170a8e3910f0bf9e40fe9403966
Krystopher Quintero <krystopher.q@gmail.com>
2022-04-27 15:00:59 -0700

    created README

---
+++ server.js
@@ -0,0 +1,24 @@
+const PORT = process.envPORT || 3001;
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+
+
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
