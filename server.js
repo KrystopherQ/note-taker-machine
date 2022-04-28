8a2b08c7c74c8e0a2522ecde009acc893f4726e3
Krystopher Quintero <krystopher.q@gmail.com>
2022-04-27 17:58:39 -0700

    can now add notes

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const PORT = process.envPORT || 3001;
 const fs = require('fs');
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
+const note = require('./db/db.json')
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,14 +14,40 @@ app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
-app.get('/', (req, res) =>
+app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
-app.get()
+app.get('/api/notes', (req, res) => {
+    res.json(note.slice(1))
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './pulbic/index.html'))
+})
+
+
+function newNote(body, noteArr) {
+    const newNotes = body;
+    if (!Array.isArray(noteArr))
+        noteArr = []
+    if (noteArr === 0)
+        noteArr.push(0)
+    body.id = noteArr[0]
+    noteArr[0]++
+        noteArr.push(newNotes)
+    fs.writeFile(path.join(__dirname, './db/db.json'), json.stringify(noteArr, null, 2))
+    return newNotes;
+}
+
+app.post('/api/notes', (req, res) => {
+    const newNotes = newNote(req.body, note)
+    res.json(newNotes)
+})
+
 
 
 
 app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT} 🚀`)
+    console.log(`App listening ${PORT} 🚀`)
 );
